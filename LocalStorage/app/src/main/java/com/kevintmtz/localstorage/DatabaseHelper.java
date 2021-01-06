250373744794b8a07d0aa42b190570b4e1e18e63
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-01-06 16:55:21 -0600

    Created LocalStorage App.

---
+++ LocalStorage/app/src/main/java/com/kevintmtz/localstorage/DatabaseHelper.java
@@ -0,0 +1,24 @@
+package com.kevintmtz.localstorage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_FILE = "StudentDatabase.db";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_FILE, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
