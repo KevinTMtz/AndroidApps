ce0ab7abe20b8a847aa662e5c8bb9ed89015cded
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-01-07 16:52:21 -0600

    LocalStorage App: DatabaseHelper & Main methods.

--- LocalStorage/app/src/main/java/com/kevintmtz/localstorage/DatabaseHelper.java
+++ LocalStorage/app/src/main/java/com/kevintmtz/localstorage/DatabaseHelper.java
@@ -1,12 +1,18 @@
 package com.kevintmtz.localstorage;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_FILE = "StudentDatabase.db";
+    private static final String TABLE = "Students";
+    private static final String FIELD_ID = "id";
+    private static final String FIELD_NAME = "name";
+    private static final String FIELD_STUDENT_ID = "studentID";
 
     public DatabaseHelper(Context context) {
         super(context, DB_FILE, null, 1);
@@ -14,11 +20,54 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE + "(" +
+                FIELD_ID + " INTEGER PRIMARY KEY, " +
+                FIELD_NAME + " TEXT, " +
+                FIELD_STUDENT_ID + " TEXT)";
 
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS ?";
+        String[] params = {TABLE};
+        db.execSQL(query, params);
+        onCreate(db);
+    }
 
+    public void save(String name, String studentID) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(FIELD_NAME, name);
+        values.put(FIELD_STUDENT_ID, studentID);
+
+        db.insert(TABLE, null, values);
+    }
+
+    public int delete(String name) {
+        SQLiteDatabase db = getWritableDatabase();
+        String clause = FIELD_NAME + " = ? ";
+        String[] params = {name};
+
+        return db.delete(TABLE, clause, params);
+    }
+
+    public int search(String name) {
+        SQLiteDatabase db = getReadableDatabase();
+        String clause = FIELD_NAME + " = ?";
+        String[] params = {name};
+
+        Cursor c = db.query(TABLE, null, clause, params, null, null, null);
+        int result = -1;
+
+        if (c.moveToFirst()) {
+            result = c.getInt(0);
+            String studentID = c.getString(2);
+        }
+
+        return result;
     }
 }
--- LocalStorage/app/src/main/java/com/kevintmtz/localstorage/MainActivity.java
+++ LocalStorage/app/src/main/java/com/kevintmtz/localstorage/MainActivity.java
@@ -3,12 +3,39 @@ package com.kevintmtz.localstorage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText id, name, studentID;
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        id = findViewById(R.id.PlainTextID);
+        name = findViewById(R.id.PlainTextName);
+        studentID = findViewById(R.id.PlainTextStudentID);
+
+        dbHelper = new DatabaseHelper(this);
+    }
+
+    public void add(View v) {
+        dbHelper.save(name.getText().toString(), studentID.getText().toString());
+        Toast.makeText(this, "Saved records!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void search(View v) {
+        int idQuery = dbHelper.search(name.getText().toString());
+        id.setText("" + idQuery);
+    }
+
+    public void delete(View v) {
+        int affected = dbHelper.delete(name.getText().toString());
+        Toast.makeText(this, "Deleted " + affected + "records", Toast.LENGTH_SHORT).show();
     }
 }
