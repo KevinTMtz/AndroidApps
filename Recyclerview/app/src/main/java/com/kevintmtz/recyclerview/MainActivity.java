c00494bbe1d1ae84d1f5786b94f10dc7ed8d894d
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-01-08 16:51:04 -0600

    Recyclerview App: Added image & text fragments.

--- Recyclerview/app/src/main/java/com/kevintmtz/recyclerview/MainActivity.java
+++ Recyclerview/app/src/main/java/com/kevintmtz/recyclerview/MainActivity.java
@@ -1,8 +1,11 @@
 package com.kevintmtz.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fragmentA(View v) {
+        ImageFragment imageFragment = new ImageFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, imageFragment, "Image Fragment");
+        fragmentTransaction.commit();
+    }
+
+    public void fragmentB(View v) {
+        TextFragment helloFragment = new TextFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, helloFragment, "Image Fragment");
+        fragmentTransaction.commit();
     }
 }
